dd1ce32be4d67ccc7d7d2a35a5164e50c664f18e
Kauan-01 <k.kantovick2008@gmail.com>
2026-07-18 11:52:55 -0300

    feat: RF13: try/catch e validação de arquivo em formato inválido

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -4,34 +4,46 @@ export async function BuscarVagas() {
 
     console.log("Carregando vagas…");
 
-    const Resposta = await fetch("./assets/data/vagas.json");
+    try {
 
-    if (!Resposta.ok) {
+        const Resposta = await fetch("./assets/data/vagas.json");
 
-        throw new Error("Erro ao carregar as vagas.");
-    
+        if (!Resposta.ok) {
+
+            throw new Error("Erro ao carregar as vagas.");
+
+        }
+
+        const VagasJson = await Resposta.json();
+
+        if (VagasJson.length === 0) {
+            console.log("Nenhuma vaga encontrada.");
+            return [];
+        }
+
+        if (!Array.isArray(VagasJson)) {
+            throw new Error("Formato inválido.");
+        }
+
+        console.log("Vagas encontradas com sucesso!");
+
+        const Vagas = VagasJson.map(vaga =>
+            new VagaTecnologia(
+                vaga.id,
+                vaga.empresa,
+                vaga.cargo,
+                vaga.requisitos,
+                vaga.salario,
+                vaga.modalidade,
+                vaga.anosExperiencia
+            )
+        );
+
+        return Vagas;
+
+    } catch (erro) {
+
+        console.error("Erro:", erro.message);
+
     }
-
-    const VagasJson = await Resposta.json();   
-
-    if (VagasJson.length === 0) {
-        console.log("Nenhuma vaga encontrada.");
-        return [];
-    }
-
-    console.log("Vagas encontradas com sucesso!");
-
-    const Vagas = VagasJson.map(vaga =>
-        new VagaTecnologia(
-            vaga.id,
-            vaga.empresa,
-            vaga.cargo,
-            vaga.requisitos,
-            vaga.salario,
-            vaga.modalidade,
-            vaga.anosExperiencia
-        )
-    );
-
-    return Vagas;
 }
