99315ef0e11d2c877acaa1fa9ca5effef6ef88d3
Kauan-01 <k.kantovick2008@gmail.com>
2026-07-19 19:47:15 -0300

    docs: passei o guia de desenvolvimento do motor.js para o arquivo README.md

--- assets/scripts/motor.js
+++ assets/scripts/motor.js
@@ -1,23 +1,9 @@
-/*
-1 - Definicao das classes (POO, metodos, heranca)
-2 - A utiizacao das classes e demais instâncias
-3 - Fluxo do sistema
-    3.1 - Capturar as vagas cadastradas (10 vagas) - async/await/fetch
-    3.2 - Contador de análises
-    3.3 - *Filtrar as vagas que são para que aquele usuario (tempo de experiencia - 3 vagas)
-    3.4 - Calcular a compatibilidade de cada vaga *filtrada para aquele usuario
-    3.5 - Classifica essa compatibilidade
-    3.6 - Faz e exibe o cálulo de compatibilidade com a vaga escolhida
-    3.7 - Destaca a melhor dentre as filtradas
-    3.8 - Recomendação de estudos das habilidades faltantes da pior vaga
-    3.9 - Callback e total de análises
-*/
-
 import { CandidatoFormulario } from "./ui.js";
 import { BuscarVagas } from "./dados.js";
 
 const Candidato_1 = await CandidatoFormulario();
 const Vagas = await BuscarVagas();
+
 //==============================CLASSE-CANDIDATO=========================================
 export class Candidato {
     constructor(nome, area, estiloTrabalho, habilidades, tempoExperiencia) {
