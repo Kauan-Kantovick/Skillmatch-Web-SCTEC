79cb4e991bfed76b7b06c2a80a3f0411d34a2f6f
Kauan-01 <k.kantovick2008@gmail.com>
2026-07-19 20:11:25 -0300

    feat: sessão de importações no main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,30 @@
+//==============================IMPORTAÇÕES=========================================
+
 import { BuscarVagas } from "./dados.js";
+
+export const FuncaoBuscarVagas = await BuscarVagas();
+
 import {CandidatoFormulario} from "./ui.js";
+
+export const FuncaoCandidatoFormulario = await CandidatoFormulario();
+
 import {Formulario} from "./ui.js";
+
+export const FuncaoFormulario = await Formulario();
+
+import { VagaTecnologia } from "./motor.js";
+
+export const ClasseVagaTecnologia = VagaTecnologia;
+
+import { SessaoCards } from "./ui.js";
+
+export const HTMLSessaoCards = await SessaoCards;
+
+import { Mensagem } from "./ui.js";
+
+export const HTMLMensagem = Mensagem;
+
+import {Candidato} from "./motor.js";
+
+export const ClasseCandidato = Candidato;
+
