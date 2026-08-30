35cb00f459a0fd17b5f88872ab5ff641b6c0f22f
Kauan K. D. da Silva <k.kantovick2008@gmail.com>
2026-08-30 20:27:04 -0300

    feat: Reafatoração da arquitetura de software - dados.js

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -1,12 +1,80 @@
-//==============================BUSCAR-VAGAS=========================================
+//==============================CLASSE-Candidato=========================================
 
-// - Importações
+export class Candidato {
+    constructor(nome, area, habilidades, estiloTrabalho, tempoExperiencia) {
+        this.nome = nome;
+        this.area = area;
+        this.habilidades = habilidades;
+        this.estiloTrabalho = estiloTrabalho;
+        this.tempoExperiencia = tempoExperiencia;
+    };
+    GetNome(){
+        return this.nome;
+    };
+    GetArea(){
+        return this.area;
+    };
+    GetEstiloTrabalho(){
+        return this.estiloTrabalho;
+    };
+    GetHabilidades(){
+        return this.habilidades;
+    };
+    GetTempoExperiencia(){
+        return this.tempoExperiencia;
+    };
+}
 
-import { VagaTecnologia } from "./motor.js";
+//==============================CLASSE-Vaga=========================================
 
-import { ExibirMensagemErro } from "./ui.js";
+class Vaga {
+    constructor(id, empresa, cargo, requisitos, salario, modeloTrabalho) {
+        this.id = id;
+        this.empresa = empresa;
+        this.cargo = cargo;
+        this.requisitos = requisitos;
+        this.salario = salario;
+        this.modeloTrabalho = modeloTrabalho;
+    };
+    GetId() {
+        return this.id;
+    };
+    GetEmpresa() {
+        return this.empresa;
+    };
+    GetCargo() {
+        return this.cargo;
+    };
+    GetRequisitos() {
+        return this.requisitos;
+    };
+    GetSalario() {
+        return this.salario;
+    };
+    GetModeloTrabalho() {
+        return this.modeloTrabalho;
+    };
+    ApresentacaoVaga() {
+        return `A empresa ${this.empresa} está contratando para o cargo de ${this.cargo} (ID: ${this.id}). Procuramos profissionais com os requisitos: ${this.requisitos}. Oferecemos salário de ${this.salario} e modelo de trabalho ${this.modeloTrabalho}.`;
+    }
+}
 
-// - Função BuscarVagas
+//==============================CLASSE-VagaTecnologia=========================================
+
+export class VagaTecnologia extends Vaga {
+    constructor(id, empresa, cargo, requisitos, salario, modeloTrabalho, anosExperiencia = 0) {
+        super(id, empresa, cargo, requisitos, salario, modeloTrabalho);
+        this.anosExperiencia = anosExperiencia;
+    };
+    GetAnosExperiencia(){
+        return this.anosExperiencia;
+    };
+    ApresentacaoVaga() {
+        return `A empresa ${this.empresa} está contratando para o cargo de ${this.cargo} (ID: ${this.id}). É necessário possuir ${this.anosExperiencia} de experiência e conhecimentos em ${this.requisitos}. Oferecemos salário de ${this.salario} e modelo de trabalho ${this.modeloTrabalho}.`;
+    }
+}
+
+//==============================BuscarVagas=========================================
 
 export async function BuscarVagas() {
 
@@ -14,22 +82,20 @@ export async function BuscarVagas() {
 
     try {
 
-        const Resposta = await fetch("/assets/data/vagas.json");;
+        const Resposta = await fetch("/assets/data/vagas.json");
 
         if (!Resposta.ok) {
-            ExibirMensagemErro(
-                "Ouver um erro ao carregar as vagas.",
-                "Texto-vermelho"
-            );
+            const MensagemErro = `Erro ao carregar as vagas`;
+            console.log(MensagemErro);
+            return MensagemErro;
         }
 
         const VagasJson = await Resposta.json();
 
         if (VagasJson.length === 0) {
-            ExibirMensagemErro(
-                "Nenhuma vaga encontrada.",
-                "Texto-vermelho"
-            );
+            const ErroVazio = `Array Vazio`;
+            console.log(ErroVazio);
+            return ErroVazio;
         }
 
         console.log("Vagas encontradas com sucesso!");
